06e6e42c0c49f779d3327af3b776e7f402ad5650
Pranali Prakash Pansare <89742255+pranalipansare99@users.noreply.github.com>
2022-10-02 15:22:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@
 import '../styles/App.css';
 import React, { useState } from 'react';
 
-export default function App() {
- //code here 
-
+// export default function App() {
+const App=()=>{
+ const[input,setInput]=useState(");
+   const[term,setTerm ]=useState(");
+    const handleChange=(e)=>{
+  setInput(e.target.value);
+ }
+ const handleSubmit=(e)=>{
+  e.preventDefault():
+  setTerm(term+input);
+  setInput(");
+           }
  
   return (
     <div>
@@ -13,9 +22,11 @@ export default function App() {
   
       <p id='intro'>Concated String</p>
       <p id='text'>{text} </p>
-      <button id='button' onClick={buttonClick}>
+      <button id='button' onClick={handleSubmit}>Submit>
         Click me
       </button>
+ <p>{term}</p>
     </div>
   );
 }
+export default App;
